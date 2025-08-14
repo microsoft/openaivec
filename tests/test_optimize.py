28d7288d3cfbd5a036e1234584ef258e2ca34e87
Hiroki Mizukami <6128022+piroyoung@users.noreply.github.com>
2025-08-15 03:06:38 +0900

    chore: fix tests

--- tests/test_optimize.py
+++ tests/test_optimize.py
@@ -34,7 +34,7 @@ class TestBatchSizeSuggester:
         assert suggester.min_batch_size == 10
         assert suggester.min_duration == 30.0
         assert suggester.max_duration == 60.0
-        assert suggester.step_ratio == 0.1
+        assert suggester.step_ratio == 0.2
         assert suggester.sample_size == 4
         assert len(suggester._history) == 0
         assert suggester._batch_size_changed_at is None
